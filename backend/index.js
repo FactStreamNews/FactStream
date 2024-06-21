a6afd203c90b7377cc73c24c5f610f9be1ed6f8e
Tom Linn <tomkyawkhinelin@gmail.com>
2024-06-21 19:44:42 -0400

    Fixed some constants and testing out routing express with postman

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,44 @@
 //index.js
-import express, {json} from 'express';
+
+import express, { json } from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import { db } from './config/firebase.js';
 import userRoutes from './userRoutes.js';
 import articleRoutes from './routes/article-routes.js';
 
-dotenv.config ();
-const app = express ();
+
+dotenv.config();
+const app = express();
 const port = process.env.PORT || 3000;
 
-app.use (cors ());
-app.use (json ());
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
-app.use (userRoutes);
+
+app.get('/', (req, res) => {
+  res.json({ mssg: 'Welcome to the app' })
+})
+
+
+app.use(cors());
+app.use(json());
+
+app.use(userRoutes);
 app.use(articleRoutes);
 
-app.use ('*', (req, res, next) => {
-  console.log (req.baseUrl, req.method);
-  next ();
+
+
+app.use((error, req, res, next) => {
+  console.error('Error encountered:', error);
+  res.status(500).json({ error: error.message });
 });
 
-app.use ((error, req, res, next) => {
-    console.error('Error encountered:', error); 
-  res.status (500).json ({error: error.message});
-});
 
-app.listen (port, () => console.log ('Express app listening on {port}'));
+app.listen(port, () => {
+  console.log('listening on port: ', port)
+}
+);
