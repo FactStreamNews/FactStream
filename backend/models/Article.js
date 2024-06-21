0c419a7f0a332a01b397b7a8f423d659f884067c
perotins <maxfuligni@gmail.com>
2024-06-21 15:03:55 -0400

    fix article obj to use firebase

--- backend/models/Article.js
+++ backend/models/Article.js
@@ -1,14 +1,25 @@
-// // models/Article.js
-// const mongoose = require('mongoose');
+// models/Article.js
+import { db } from '../firebase.js';
 
-// // const articleSchema = new mongoose.Schema({
-// //   title: String,
-// //   content: String,
-// //   link: String,
-// //   pubDate: Date,
-// //   source: String,
-// // });
+const articlesCollection = db.collection('articles');
+// new article
+export const createArticle = async (article) => {
+  const res = await articlesCollection.add(article);
+  return res.id;
+};
 
-// module.exports = mongoose.model('Article', articleSchema);
+// getter
+export const getArticleById = async (id) => {
+  const doc = await articlesCollection.doc(id).get();
+  if (!doc.exists) {
+    throw new Error('Article not found');
+  }
+  return doc.data();
+};
 
-// The above is for Mongo only i think
+// update for likes count etc
+export const updateArticle = async (id, updatedArticle) => {
+  await articlesCollection.doc(id).set(updatedArticle, { merge: true });
+};
+
+
