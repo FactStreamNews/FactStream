42b2f727d1368d17205de384be9ce104f7cecb10
realBrando <brandonandrew0@gmail.com>
2024-07-03 12:03:43 -0400

    Preferences set appear in navbar, still minor refresh bug

--- frontend/src/contexts/PageLayout.jsx
+++ frontend/src/contexts/PageLayout.jsx
@@ -15,6 +15,13 @@ import { useEffect, useState } from 'react';
 const Navbar = () => {
   const [user, loading, error] = useAuthState(auth);
   const [isAdmin, setIsAdmin] = useState(false);
+  const [techPref, setTechPref] = useState(false);
+  const [healthPref, setHealthPref] = useState(false);
+  const [sportsPref, setSportsPref] = useState(false);
+  const [travelPref, setTravelPref] = useState(false);
+  const [politicsPref, setPoliticsPref] = useState(false);
+  const [sciencePref, setSciencePref] = useState(false);
+
   useEffect(() => {
     const fetchAdminStatus = async () => {
       if (user) {
@@ -23,6 +30,18 @@ const Navbar = () => {
         // Query to find the document with the uid field
         const q = query(collection(db, 'users'), where('uid', '==', user.uid));
         const querySnapshot = await getDocs(q);
+        const doc1 = await getDocs(q);
+        const data = doc1.docs[0].data();
+        const docID = doc1.docs[0].id;
+        const docRef = doc(db, "users", docID);
+
+        //Check the user pref values
+        setTechPref(data.techPreference || false);
+        setHealthPref(data.healthPreference || false);
+        setSportsPref(data.sportsPreference || false);
+        setTravelPref(data.travelPreference || false);
+        setPoliticsPref(data.politicsPreference || false);
+        setSciencePref(data.sciencePreference || false);
         
         if (!querySnapshot.empty) {
           const userDoc = querySnapshot.docs[0];
@@ -35,7 +54,7 @@ const Navbar = () => {
     };
 
     fetchAdminStatus();
-  }, [user]);
+  }, [user, loading]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -51,6 +70,26 @@ const Navbar = () => {
         <Typography variant="h6" component="div">
           <Button component={Link} to="/" color="inherit">FactStream</Button>
         </Typography>
+        <div>
+         {user && techPref && (
+          <Button component={Link} to="/tech" color="inherit">Tech</Button>
+         )}
+         {user && healthPref && (
+          <Button component={Link} to="/health" color="inherit">Health</Button>
+         )}
+         {user && politicsPref && (
+          <Button component={Link} to="/politics" color="inherit">Politics</Button>
+         )}
+         {user && sportsPref && (
+          <Button component={Link} to="/sports" color="inherit">Sports</Button>
+         )}
+         {user && sciencePref && (
+          <Button component={Link} to="/science" color="inherit">Science</Button>
+         )}
+         {user && travelPref && (
+          <Button component={Link} to="/travel" color="inherit">Travel</Button>
+         )}
+         </div>
         <div>
         {user && (
             <Button component={Link} to="/admin" color="inherit"> {isAdmin && "ADMIN"}</Button>
