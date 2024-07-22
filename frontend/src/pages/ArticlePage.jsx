b3e28ead625a2db935e8764d9d16e7263a9ca3ba
Tom Linn <tomkyawkhinelin@gmail.com>
2024-07-21 21:23:57 -0400

    activity history

--- frontend/src/pages/ArticlePage.jsx
+++ frontend/src/pages/ArticlePage.jsx
@@ -81,9 +81,6 @@ const ArticlePage = () => {
       alert('Please sign in to comment.');
       return;
     }
-
-
-
     try {
       const commentsRef = collection(db, 'articles', id, 'comments');
       const q = query(collection(db, "users"), where("uid", "==", user?.uid));
@@ -102,6 +99,17 @@ const ArticlePage = () => {
       const commentsSnap = await getDocs(commentsRef);
       const commentsList = commentsSnap.docs.map(doc => ({ id: doc.id, ...doc.data() }));
       setComments(commentsList);
+
+      // Record activity
+    const activityRef = collection(db, 'activity'); // Reference to the activity collection
+    await addDoc(activityRef, {
+      description: `${name} commented on ${article.title}`,
+      userId: user.uid,
+      articleId: id,
+      type: 'comment',
+      timestamp: new Date()
+    });
+
     } catch (error) {
       console.error('Error adding comment:', error);
     }
@@ -174,183 +182,225 @@ const ArticlePage = () => {
       alert('Please sign in to like articles.');
       return;
     }
-      const likesRef = collection(db, 'likes');
-      const articleRef = doc(db, 'articles', id);
-      const userLikeRef = query(likesRef, where('userId', '==', user.uid), where('articleId', '==', id));
-      const userLikeSnapshot = await getDocs(userLikeRef);
 
-      try {
-        if (userLikeSnapshot.empty) {
-          // User is liking the article
-          await addDoc(likesRef, {
-            userId: user.uid,
-            articleId: id
-          });
-          await updateDoc(articleRef, {
-            likes: likes + 1
-          });
-          setLikes(likes + 1);
-          setHasLiked(true);
-        } else {
-          // User is unliking the article
-          const likeDocId = userLikeSnapshot.docs[0].id;
-          const likeDocRef = doc(db, 'likes', likeDocId);
-          await deleteDoc(likeDocRef);
-          await updateDoc(articleRef, {
-            likes: likes - 1
-          });
-          setLikes(likes - 1);
-          setHasLiked(false);
-        }
-      } catch (error) {
-        console.error('Error updating likes:', error);
+    const likesRef = collection(db, 'likes');
+    const activityRef = collection(db, 'activity'); // Reference to the activity collection
+    const articleRef = doc(db, 'articles', id);
+    const userLikeRef = query(likesRef, where('userId', '==', user.uid), where('articleId', '==', id));
+    const userLikeSnapshot = await getDocs(userLikeRef);
+
+    try {
+      if (userLikeSnapshot.empty) {
+        // User is liking the article
+        await addDoc(likesRef, {
+          userId: user.uid,
+          articleId: id
+        });
+        await updateDoc(articleRef, {
+          likes: likes + 1
+        });
+        setLikes(likes + 1);
+        setHasLiked(true);
+
+        // Record activity
+        await addDoc(activityRef, {
+          description: `${name} liked ${article.title}`,
+          userId: user.uid,
+          articleId: id,
+          type: 'like',
+          timestamp: new Date()
+        });
+      } else {
+        // User is unliking the article
+        const likeDocId = userLikeSnapshot.docs[0].id;
+        const likeDocRef = doc(db, 'likes', likeDocId);
+        await deleteDoc(likeDocRef);
+        await updateDoc(articleRef, {
+          likes: likes - 1
+        });
+        setLikes(likes - 1);
+        setHasLiked(false);
+
+        // Record activity
+        await addDoc(activityRef, {
+          description: `${name} removed like for ${article.title}`,
+          userId: user.uid,
+          articleId: id,
+          type: 'unlike',
+          timestamp: new Date()
+        });
       }
-    };
-
-    const handleDislike = async () => {
-      if (!user) {
-        alert('Please sign in to dislike articles.');
-        return;
-      }
-        const dislikesRef = collection(db, 'dislikes');
-        const articleRef = doc(db, 'articles', id);
-        const userDislikeRef = query(dislikesRef, where('userId', '==', user.uid), where('articleId', '==', id));
-        const userDislikeSnapshot = await getDocs(userDislikeRef);
-  
-        try {
-          if (userDislikeSnapshot.empty) {
-            // User is liking the article
-            await addDoc(dislikesRef, {
-              userId: user.uid,
-              articleId: id
-            });
-            await updateDoc(articleRef, {
-              dislikes: dislikes + 1
-            });
-            setDislikes(dislikes + 1);
-            setHasDisliked(false);
-          } else {
-            // User is unliking the article
-            const dislikeDocId = userDislikeSnapshot.docs[0].id;
-            const dislikeDocRef = doc(db, 'dislikes', dislikeDocId);
-            await deleteDoc(dislikeDocRef);
-            await updateDoc(articleRef, {
-              dislikes: dislikes - 1
-            });
-            setDislikes(dislikes - 1);
-            setHasDisliked(false);
-          }
-        } catch (error) {
-          console.error('Error updating dislikes:', error);
-        }
-      };
-
-
-
-    const toggleSave = async (index) => {
-      if (!user) return;
-
-      const articleId = id;
-      console.log("here");
-      console.log("articleID:", articleId);
-      const q = query(collection(db, "users"), where("uid", "==", user?.uid));
-      const doc1 = await getDocs(q);
-      const docId = doc1.docs[0].id;
-      const userDocRef = doc(db, "users", docId);
-      console.log("userDocRef", userDocRef);
-
-      try {
-        if (savedArticles.includes(articleId)) {
-          await updateDoc(userDocRef, {
-            savedArticles: arrayRemove(articleId)
-          });
-          setSavedArticles(prevSavedArticles => prevSavedArticles.filter(id => id !== articleId));
-          setHasSaved(false);
-        } else {
-          await updateDoc(userDocRef, {
-            savedArticles: arrayUnion(articleId)
-          });
-          setSavedArticles(prevSavedArticles => [...prevSavedArticles, articleId]);
-          setHasSaved(true);
-        }
-      } catch (error) {
-        console.error('Error updating saved articles:', error);
-      }
-    };
-
-    if (!article) return <div>Loading...</div>;
-
-    return (
-      <div className="article-page">
-        <div className="article-content">
-          <h1>{article.title}</h1>
-          <img src={article.imgUrl} alt={article.title} className="article-image" />
-          <div className='button-container'>
-            <button
-              className={`like-button ${hasLiked ? 'liked' : ''}`}
-              onClick={handleLike}
-            >
-              {hasLiked ? 'Liked' : 'Like'} ({likes})
-            </button>
-
-            <button
-              className={`dislike-button ${hasDisliked ? 'disliked' : ''}`}
-              onClick={handleDislike}
-            >
-              {hasDisliked ? 'disliked' : 'dislike'} ({dislikes})
-            </button>
-
-            <button
-              className="save-button"
-              onClick={toggleSave}
-            >
-              {hasSaved ? 'Unsave' : 'Save'}
-            </button>
-          </div>
-
-          <div
-            dangerouslySetInnerHTML={{ __html: article.content }}
-          />
-          
-
-          <div className="comments-section">
-            <h2>Comments</h2>
-            {comments.map((comment) => (
-              <div key={comment.id} className="comment">
-                <p>
-                  <strong>
-                    {comment.isPrivate ? 'Anonymous' : <Link to={`/profile/${comment.userId}`}>{comment.userName}</Link>}
-                  </strong>
-                  ({new Date(comment.createdAt.toDate()).toLocaleString()}): {comment.text}
-                </p>
-              </div>
-            ))}
-            <form onSubmit={handleAddComment}>
-              <textarea
-                value={newComment}
-                onChange={(e) => setNewComment(e.target.value)}
-                placeholder="Add a comment"
-                required
-              />
-              <button type="submit">Submit</button>
-              <button type="button" onClick={handleCancelComment}>Cancel</button>
-            </form>
-          </div>
-
-        </div>
-        <div className="related-articles">
-          <h2>Related Articles</h2>
-          {relatedArticles.map((relatedArticle, index) => (
-            <div key={index} className="related-article-item">
-              <Link to={`/article/${relatedArticle.id}`}>
-                <h3>{relatedArticle.title}</h3>
-                <img src={relatedArticle.imgUrl} alt={relatedArticle.title} className="related-article-image" />
-              </Link>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
+    } catch (error) {
+      console.error('Error updating likes:', error);
+    }
   };
 
-  export default ArticlePage;
+
+  const handleDislike = async () => {
+    if (!user) {
+      alert('Please sign in to dislike articles.');
+      return;
+    }
+  
+    const dislikesRef = collection(db, 'dislikes');
+    const activityRef = collection(db, 'activity'); // Reference to the activity collection
+    const articleRef = doc(db, 'articles', id);
+    const userDislikeRef = query(dislikesRef, where('userId', '==', user.uid), where('articleId', '==', id));
+    const userDislikeSnapshot = await getDocs(userDislikeRef);
+  
+    try {
+      if (userDislikeSnapshot.empty) {
+        // User is disliking the article
+        await addDoc(dislikesRef, {
+          userId: user.uid,
+          articleId: id
+        });
+        await updateDoc(articleRef, {
+          dislikes: dislikes + 1
+        });
+        setDislikes(dislikes + 1);
+        setHasDisliked(true);
+  
+        // Record activity
+        await addDoc(activityRef, {
+          description: `${name} disliked ${article.title}`,
+          userId: user.uid,
+          articleId: id,
+          type: 'dislike',
+          timestamp: new Date()
+        });
+      } else {
+        // User is removing dislike from the article
+        const dislikeDocId = userDislikeSnapshot.docs[0].id;
+        const dislikeDocRef = doc(db, 'dislikes', dislikeDocId);
+        await deleteDoc(dislikeDocRef);
+        await updateDoc(articleRef, {
+          dislikes: dislikes - 1
+        });
+        setDislikes(dislikes - 1);
+        setHasDisliked(false);
+  
+        // Record activity
+        await addDoc(activityRef, {
+          description: `${name} removed dislike from ${article.title}`,
+          userId: user.uid,
+          articleId: id,
+          type: 'remove_dislike',
+          timestamp: new Date()
+        });
+      }
+    } catch (error) {
+      console.error('Error updating dislikes:', error);
+    }
+  };
+  
+
+
+
+  const toggleSave = async (index) => {
+    if (!user) return;
+
+    const articleId = id;
+    console.log("here");
+    console.log("articleID:", articleId);
+    const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+    const doc1 = await getDocs(q);
+    const docId = doc1.docs[0].id;
+    const userDocRef = doc(db, "users", docId);
+    console.log("userDocRef", userDocRef);
+
+    try {
+      if (savedArticles.includes(articleId)) {
+        await updateDoc(userDocRef, {
+          savedArticles: arrayRemove(articleId)
+        });
+        setSavedArticles(prevSavedArticles => prevSavedArticles.filter(id => id !== articleId));
+        setHasSaved(false);
+      } else {
+        await updateDoc(userDocRef, {
+          savedArticles: arrayUnion(articleId)
+        });
+        setSavedArticles(prevSavedArticles => [...prevSavedArticles, articleId]);
+        setHasSaved(true);
+      }
+    } catch (error) {
+      console.error('Error updating saved articles:', error);
+    }
+  };
+
+  if (!article) return <div>Loading...</div>;
+
+  return (
+    <div className="article-page">
+      <div className="article-content">
+        <h1>{article.title}</h1>
+        <img src={article.imgUrl} alt={article.title} className="article-image" />
+        <div className='button-container'>
+          <button
+            className={`like-button ${hasLiked ? 'liked' : ''}`}
+            onClick={handleLike}
+          >
+            {hasLiked ? 'Liked' : 'Like'} ({likes})
+          </button>
+
+          <button
+            className={`dislike-button ${hasDisliked ? 'disliked' : ''}`}
+            onClick={handleDislike}
+          >
+            {hasDisliked ? 'disliked' : 'dislike'} ({dislikes})
+          </button>
+
+          <button
+            className="save-button"
+            onClick={toggleSave}
+          >
+            {hasSaved ? 'Unsave' : 'Save'}
+          </button>
+        </div>
+
+        <div
+          dangerouslySetInnerHTML={{ __html: article.content }}
+        />
+
+
+        <div className="comments-section">
+          <h2>Comments</h2>
+          {comments.map((comment) => (
+            <div key={comment.id} className="comment">
+              <p>
+                <strong>
+                  {comment.isPrivate ? 'Anonymous' : <Link to={`/profile/${comment.userId}`}>{comment.userName}</Link>}
+                </strong>
+                ({new Date(comment.createdAt.toDate()).toLocaleString()}): {comment.text}
+              </p>
+            </div>
+          ))}
+          <form onSubmit={handleAddComment}>
+            <textarea
+              value={newComment}
+              onChange={(e) => setNewComment(e.target.value)}
+              placeholder="Add a comment"
+              required
+            />
+            <button type="submit">Submit</button>
+            <button type="button" onClick={handleCancelComment}>Cancel</button>
+          </form>
+        </div>
+
+      </div>
+      <div className="related-articles">
+        <h2>Related Articles</h2>
+        {relatedArticles.map((relatedArticle, index) => (
+          <div key={index} className="related-article-item">
+            <Link to={`/article/${relatedArticle.id}`}>
+              <h3>{relatedArticle.title}</h3>
+              <img src={relatedArticle.imgUrl} alt={relatedArticle.title} className="related-article-image" />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ArticlePage;
